19558e50d5194aeb9606cace25554a9b697c5e86
Mohd Sahil <home@Mohds-MacBook.local>
2018-10-21 06:53:09 +0530

    Basic Setup

---
+++ burger-app-react16/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+
+export default class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Buger Image</div>
+        <div>Buger Control</div>
+      </Aux>
+    );
+  }
+}
